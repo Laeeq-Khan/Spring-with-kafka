a702fde80829ade77a9ee919f12098a89f48e573
Muhammad Laeeq Uz Zaman Khan Niazi <laiqkhan0000@gmail.com>
2025-08-28 12:00:08 +0500

    First commit for projects creation

---
+++ Stock Service/stock-service/src/test/java/com/laeeq/stock_service/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laeeq.stock_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
