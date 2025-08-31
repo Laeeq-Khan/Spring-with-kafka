5282278c6d9d3ad6990862d8a23f66046017b27c
Muhammad Laeeq Uz Zaman Khan Niazi <laiqkhan0000@gmail.com>
2025-08-31 13:51:54 +0500

    add comments in the code

--- Order Service/order-service/src/main/java/com/laeeq/order_service/kafka/OrderProducer.java
+++ Order Service/order-service/src/main/java/com/laeeq/order_service/kafka/OrderProducer.java
@@ -2,19 +2,21 @@ package com.laeeq.order_service.kafka;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
-
-import com.laeeq.base_domains.dto.Order;
 import com.laeeq.base_domains.dto.OrderEvent;
 
 @Service
 public class OrderProducer {
 
     @Autowired
+    // in this case no need becuase we have single bean in kafkatopicconfiguration class, 
+    //if we have different beans with same data type we do use qulifers
+    @Qualifier("topic") 
     private NewTopic topic;
 
     private KafkaTemplate<String, OrderEvent> kafkaTemplate;
